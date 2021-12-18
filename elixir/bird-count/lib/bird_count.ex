ed0c9ecfd59786d5e6012b39eb6149de1321b811
Sreetam Das <sreetamdas@gmail.com>
2021-12-18 22:56:47 +0530

    improvement: use recursion!

--- elixir/bird-count/lib/bird_count.ex
+++ elixir/bird-count/lib/bird_count.ex
@@ -1,29 +1,18 @@
 defmodule BirdCount do
-  def today(list) do
-    list
-    |> Enum.at(0, nil)
-  end
+  def today([]), do: nil
+  def today([t | _]), do: t
 
-  def increment_day_count(list) when length(list) == 0 do
-    [1]
-  end
+  def increment_day_count([]), do: [1]
+  def increment_day_count([t | r]), do: [t + 1 | r]
 
-  def increment_day_count(list) do
-    list
-    |> List.update_at(0, &(&1 + 1))
-  end
+  def has_day_without_birds?([]), do: false
+  def has_day_without_birds?([0 | _]), do: true
+  def has_day_without_birds?([_ | r]), do: has_day_without_birds?(r)
 
-  def has_day_without_birds?(list) do
-    list
-    |> Enum.any?(fn x -> x == 0 end)
-  end
+  def total([]), do: 0
+  def total([t | r]), do: t + total(r)
 
-  def total(list) do
-    list
-    |> Enum.sum()
-  end
-
-  def busy_days(list) do
-    list |> Enum.count(fn x -> x >= 5 end)
-  end
+  def busy_days([]), do: 0
+  def busy_days([t | r]) when t >= 5, do: 1 + busy_days(r)
+  def busy_days([_ | r]), do: busy_days(r)
 end
