caf0e8bfa086aaae24c9600fc24dad1578d16943
Sreetam Das <sreetamdas@gmail.com>
2023-08-24 11:45:07 +0530

    add binary-search

--- elixir/binary-search/lib/binary_search.ex
+++ elixir/binary-search/lib/binary_search.ex
@@ -19,11 +19,40 @@ defmodule BinarySearch do
 
   @spec search(tuple, integer) :: {:ok, integer} | :not_found
   def search({}, _), do: :not_found
-  def search({number}, number), do: {:ok, 0}
+  def search({key}, key), do: {:ok, 0}
 
-  # def search(numbers, key) do
-  #   numbers
-  #   |> Tuple.to_list()
-  #   |> Enum.split()
-  # end
+  def search(numbers, key) do
+    list = numbers |> Tuple.to_list()
+    first = 0
+    last = length(list) - 1
+
+    binary_search(numbers, key, first, last)
+  end
+
+  defp binary_search(numbers, key, first, last) do
+    mid = floor((last + first) / 2)
+
+    cond do
+      key < elem(numbers, first) or key > elem(numbers, last) ->
+        :not_found
+
+      key == elem(numbers, first) ->
+        {:ok, first}
+
+      key == elem(numbers, last) ->
+        {:ok, last}
+
+      key == elem(numbers, mid) ->
+        {:ok, mid}
+
+      key < elem(numbers, mid) and mid != last ->
+        binary_search(numbers, key, first, mid)
+
+      key > elem(numbers, mid) and mid != first ->
+        binary_search(numbers, key, mid, last)
+
+      true ->
+        :not_found
+    end
+  end
 end
